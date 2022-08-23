e78006375b8e934aa2532205b2a0cc04ed2e4f51
oshadaera68 <mindartlk010@gmail.com>
2022-08-23 12:28:34 +0530

    post model completed

---
+++ Model/post.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const { post } = require('../Routes/user')
+
+const postSchema = new mongoose.Schema({
+    userId:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        required:true
+    },
+    time:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true
+    }
+})
+
+module.exports = mongoose.model('Post',postSchema)
